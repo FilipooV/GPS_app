b172330e4fb150bb8808236ac639e9c986e359df
trzmiel.filip <filiptrzmiel@gmail.com>
2023-12-12 15:06:36 +0100

    add files

---
+++ app/src/main/java/com/example/gps_app/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.gps_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.Manifest;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textviewLatitude;
+    TextView textviewLongtitude;
+
+    Button button_show;
+    LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_show = findViewById(R.id.button);
+        textviewLatitude = findViewById(R.id.textView);
+        textviewLongtitude = findViewById(R.id.textView2);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        button_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(@NonNull Location location) {
+                        textviewLatitude.setText(String.valueOf(location.getLatitude()));
+                        textviewLongtitude.setText(String.valueOf(location.getLongitude()));
+                    }
+                });
+            }
+        });
+    }
+}
+
+
+
